e6ececbe6b9200e5689d94ba372cce6bda0e8e02
Roman Gromov <hanaevroman@gmail.com>
2019-06-11 18:45:37 +0000

    Resolve "Design / markup fixes"

--- src/components/messages/messages.module.ts
+++ src/components/messages/messages.module.ts
@@ -6,12 +6,14 @@ import { TopicsModule } from 'src/components/topics/topics.module';
 import { MessagesServiceProvider, messagesSocketServiceProvider } from 'src/services/messages/provider';
 import { TopicsServiceProvider } from 'src/services/topics/provider';
 import { CreateMessageComponent } from './create-message/create-message.component';
+import { ImagePreviewComponent } from './create-message/image-preview/image-preview.component';
 import { MessagesListComponent } from './messages-list/messages-list.component';
 
 @NgModule({
   declarations: [
     MessagesListComponent,
-    CreateMessageComponent
+    CreateMessageComponent,
+    ImagePreviewComponent
   ],
   imports: [
     CommonModule,
@@ -19,6 +21,9 @@ import { MessagesListComponent } from './messages-list/messages-list.component';
     FormsModule,
 
     TopicsModule
+  ],
+  entryComponents: [
+    ImagePreviewComponent
   ],
   exports: [
     MessagesListComponent,
